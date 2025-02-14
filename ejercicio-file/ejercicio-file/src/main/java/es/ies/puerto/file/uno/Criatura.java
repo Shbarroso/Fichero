a3d759906909fe4dc57fc7b28eec7cd9bcdd6358
Shbarroso <samuelaurix2@gmail.com>
2025-02-14 20:27:36 +0000

    SE sube los comentarios de los metodos

--- ejercicio-file/ejercicio-file/src/main/java/es/ies/puerto/file/uno/Criatura.java
+++ ejercicio-file/ejercicio-file/src/main/java/es/ies/puerto/file/uno/Criatura.java
@@ -1,11 +1,39 @@
 package es.ies.puerto.file.uno;
-
+import java.util.Objects;
+/**
+ * @author Shbarroso
+ * @version 1.0.0
+ */
 public class Criatura {
     private String id;
     private String nombre;
     private String descripcion;
     private String categoria;
 
+    /*+
+     * Constructor con un solo parametro
+     */
+    public Criatura(String id) {
+        this.id = id;
+    }
+    /**
+     * Constructor con todos los parametros.
+     * @param id
+     * @param nombre
+     * @param descripcion
+     * @param categoria
+     */
+    public Criatura(String id, String nombre, String descripcion, String categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+    }
+    
+    /**
+     * Getter and Setter
+     * @return
+     */
     public String getId() {
         return id;
     }
@@ -37,4 +65,30 @@ public class Criatura {
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
+    /**
+     * ToString
+     */
+    @Override
+    public String toString() {
+        return getId() + getNombre() + getDescripcion() + getCategoria();
+    }
+    /**
+     * Equals
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Criatura)) {
+            return false;
+        }
+        Criatura criatura = (Criatura) o;
+        return Objects.equals(id, criatura.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, descripcion, categoria);
+    }
+
 }
--- ejercicio-file/ejercicio-file/src/main/java/es/ies/puerto/file/uno/FileXmlCriatura.java
+++ ejercicio-file/ejercicio-file/src/main/java/es/ies/puerto/file/uno/FileXmlCriatura.java
@@ -1,20 +1,57 @@
 package es.ies.puerto.file.uno;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
-public class FileXmlCriatura {
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.w3c.dom.NodeList;
+
+public class FileXmlCriatura {
+    static List<Criatura> criaturas = new ArrayList<>();
+    public static void main(String[] args) {
+        File archivo = new File("src/main/resources/uno.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(archivo);
+
+        
+    }
+    /**
+     * Metodo para obtener las criaturas de la lista.
+     * @return una lista con las criaturas.
+     */
     public List<Criatura> obtenerCriaturas() {
         return null;
     }
+    /**
+     * Metodo para obtener una criatura de una lista.
+     * @param criatura
+     * @return
+     */
     public Criatura obtener(Criatura criatura) {
         return null;
     }
+    /**
+     * Metodo para añadir una criatura a una lista.
+     * @param criatura
+     */
     public void addCriatura(Criatura criatura) {
     }
+    /**
+     * Metodo para eliminar una criatura de la lisa.
+     * @param criatura
+     */
     public void deleteCriatura(Criatura criatura) {
 
     }
+    /**
+     * Metodo para aptualizar una criatura en la lista.
+     * @param criatura
+     */
     public void updateCriatura(Criatura criatura) {
 
     }
