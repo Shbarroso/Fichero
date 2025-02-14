3e577ea748f96ba7e0b2706dcf2482ac72b718df
Shbarroso <samuelaurix2@gmail.com>
2025-02-14 20:11:38 +0000

    Se sube tarea

---
+++ ejercicio-file/ejercicio-file/src/main/java/es/ies/puerto/file/uno/Criatura.java
@@ -0,0 +1,40 @@
+package es.ies.puerto.file.uno;
+
+public class Criatura {
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private String categoria;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
---
+++ ejercicio-file/ejercicio-file/src/main/java/es/ies/puerto/file/uno/FileXmlCriatura.java
@@ -0,0 +1,21 @@
+package es.ies.puerto.file.uno;
+
+import java.util.List;
+
+public class FileXmlCriatura {
+
+    public List<Criatura> obtenerCriaturas() {
+        return null;
+    }
+    public Criatura obtener(Criatura criatura) {
+        return null;
+    }
+    public void addCriatura(Criatura criatura) {
+    }
+    public void deleteCriatura(Criatura criatura) {
+
+    }
+    public void updateCriatura(Criatura criatura) {
+
+    }
+}
