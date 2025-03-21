6ab4fa11fa7cd26cdd2b64340c675cef049188f7
Shbarroso <samuelaurix2@gmail.com>
2025-03-21 20:13:07 +0000

    a

---
+++ primeraa-app-fx/src/main/java/es/ies/puerto/abstracta/AbstractController.java
@@ -0,0 +1,18 @@
+package es.ies.puerto.abstracta;
+
+import es.ies.puerto.config.ConfigManager;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class AbstractController {
+    @FXML
+    public Text textUsuario;
+
+    @FXML
+    public Text textContrasenia;
+
+    public void cambiarIdioma() {
+        textUsuario.setText(ConfigManager.ConfigProperties.getProperty("textUsuario"));
+        textContrasenia.setText(ConfigManager.ConfigProperties.getProperty("textContrasenia"));
+    }
+}
